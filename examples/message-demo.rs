c24628a5a981891633de8c57835a4c5e5e7e74a8
Armin Ronacher <armin.ronacher@active-4.com>
2018-04-12 10:59:49 +0200

    fix: Fixed message demo

--- examples/message-demo.rs
+++ examples/message-demo.rs
@@ -3,7 +3,7 @@ extern crate sentry;
 fn main() {
     let _sentry = sentry::init("https://a94ae32be2584e0bbd7a4cbb95971fee@sentry.io/1041156");
     sentry::configure_scope(|scope| {
-        scope.set_fingerprint(Some(vec!["a-message".to_string()]));
+        scope.set_fingerprint(Some(["a-message"].as_ref()));
         scope.set_tag("foo", "bar");
     });
 
