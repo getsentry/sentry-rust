38f16b77698c896b5c6559325ff9b01d0187d736
Armin Ronacher <armin.ronacher@active-4.com>
2018-06-17 23:22:03 +0200

    feat: Expose breadcrumbs toplevel and add tests

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -17,3 +17,47 @@ fn test_basic_capture_message() {
         vec![("worker".to_string(), "worker1".to_string())]
     );
 }
+
+#[test]
+fn test_breadcrumbs() {
+    let events = sentry::test::with_captured_events(|| {
+        sentry::add_breadcrumb(|| sentry::Breadcrumb {
+            ty: "log".into(),
+            message: Some("First breadcrumb".into()),
+            ..Default::default()
+        });
+        sentry::add_breadcrumb(sentry::Breadcrumb {
+            ty: "log".into(),
+            message: Some("Second breadcrumb".into()),
+            ..Default::default()
+        });
+        sentry::add_breadcrumb(|| {
+            vec![
+                sentry::Breadcrumb {
+                    ty: "log".into(),
+                    message: Some("Third breadcrumb".into()),
+                    ..Default::default()
+                },
+                sentry::Breadcrumb {
+                    ty: "log".into(),
+                    message: Some("Fourth breadcrumb".into()),
+                    ..Default::default()
+                },
+            ]
+        });
+        sentry::add_breadcrumb(|| None);
+        sentry::capture_message("Hello World!", sentry::Level::Warning);
+    });
+    assert_eq!(events.len(), 1);
+    let event = events.into_iter().next().unwrap();
+
+    let messages: Vec<_> = event.breadcrumbs.iter().map(|x| {
+        (x.message.as_ref().map(|x| x.as_str()).unwrap(), x.ty.as_str())
+    }).collect();
+    assert_eq!(messages, vec![
+        ("First breadcrumb", "log"),
+        ("Second breadcrumb", "log"),
+        ("Third breadcrumb", "log"),
+        ("Fourth breadcrumb", "log"),
+    ]);
+}
