923f45efca5ce451e68f1ae43bb3332bba4082f9
Armin Ronacher <armin.ronacher@active-4.com>
2018-06-23 21:27:37 +0200

    ref: Refactor event processors to not need unsafe within sentry

--- examples/event-processors.rs
+++ examples/event-processors.rs
@@ -5,14 +5,14 @@ fn main() {
         sentry::Client::from_config("https://a94ae32be2584e0bbd7a4cbb95971fee@sentry.io/1041156");
     let _sentry = sentry::init(client);
 
-    sentry::Hub::current().add_event_processor(Box::new(|| {
+    sentry::Hub::current().add_event_processor(|| {
         let req = sentry::protocol::Request {
             url: Some("https://example.com/".parse().unwrap()),
             method: Some("GET".into()),
             ..Default::default()
         };
         Box::new(move |event| event.request = Some(req.clone()))
-    }));
+    });
 
     sentry::configure_scope(|scope| {
         scope.set_fingerprint(Some(["a-message"].as_ref()));
