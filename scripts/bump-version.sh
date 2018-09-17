6a8943f6fef4878a77e1c1ad786a48ee710fd1c7
Armin Ronacher <armin.ronacher@active-4.com>
2018-09-17 22:22:00 +0200

    feat: Added bump version script

---
+++ scripts/bump-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+echo "Current version: ${OLD_VERSION}"
+echo "Bumping version: ${NEW_VERSION}"
+
+VERSION_RE=${VERSION//\./\\.}
+find . -name Cargo.toml -type f -exec sed -i '' -e "1,/^version/ s/^version.*/version = \"${NEW_VERSION}\"/" {} \;
+cargo update -p sentry
