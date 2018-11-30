89ea80bf834d65262da44a3bb13bc39fe6bda683
Armin Ronacher <armin.ronacher@active-4.com>
2018-12-01 00:45:16 +0100

    ref: code style

--- examples/log-demo.rs
+++ examples/log-demo.rs
@@ -3,17 +3,17 @@ extern crate log;
 #[macro_use]
 extern crate sentry;
 
+use sentry::{integrations::log::LogIntegration, ClientOptions};
+
 fn main() {
     let _sentry = sentry::init(
-        sentry::ClientOptions {
+        ClientOptions {
             dsn: "https://a94ae32be2584e0bbd7a4cbb95971fee@sentry.io/1041156"
                 .parse()
                 .ok(),
             release: sentry_crate_release!(),
             ..Default::default()
-        }.add_integration(
-            sentry::integrations::log::LogIntegration::default().with_env_logger_dest(None),
-        ),
+        }.add_integration(LogIntegration::default().with_env_logger_dest(None)),
     );
 
     debug!("System is booting");
