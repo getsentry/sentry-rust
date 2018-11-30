9d148e5a6d49eb75422f9bf6f18752c368393fc8
Armin Ronacher <armin.ronacher@active-4.com>
2018-12-01 00:44:01 +0100

    ref: refactored log-demo

--- examples/log-demo.rs
+++ examples/log-demo.rs
@@ -1,23 +1,20 @@
 #[macro_use]
 extern crate log;
-extern crate pretty_env_logger;
 #[macro_use]
 extern crate sentry;
 
 fn main() {
-    let mut log_builder = pretty_env_logger::formatted_builder().unwrap();
-    log_builder.parse("info");
-    let logger = log_builder.build();
-
-    let _sentry = sentry::init((
-        "https://a94ae32be2584e0bbd7a4cbb95971fee@sentry.io/1041156",
+    let _sentry = sentry::init(
         sentry::ClientOptions {
+            dsn: "https://a94ae32be2584e0bbd7a4cbb95971fee@sentry.io/1041156"
+                .parse()
+                .ok(),
             release: sentry_crate_release!(),
             ..Default::default()
         }.add_integration(
-            sentry::integrations::log::LogIntegration::default().with_env_logger_dest(Some(logger)),
+            sentry::integrations::log::LogIntegration::default().with_env_logger_dest(None),
         ),
-    ));
+    );
 
     debug!("System is booting");
     info!("System is booting");
