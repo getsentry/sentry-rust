74bf47295ed70d6673d97cc6c3b3e527022e318b
Armin Ronacher <armin.ronacher@active-4.com>
2019-01-06 21:35:31 +0100

    gh-pages: update

--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sentry"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.5/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"sentry/integrations/log/struct.Logger.html\" title=\"struct sentry::integrations::log::Logger\">Logger</a>",synthetic:false,types:["sentry::integrations::log::Logger"]},];
+implementors["sentry"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.6/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"sentry/integrations/log/struct.Logger.html\" title=\"struct sentry::integrations::log::Logger\">Logger</a>",synthetic:false,types:["sentry::integrations::log::Logger"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/std/panic/trait.RefUnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sentry"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"sentry/struct.ClientOptions.html\" title=\"struct sentry::ClientOptions\">ClientOptions</a>",synthetic:false,types:["sentry::client::ClientOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
