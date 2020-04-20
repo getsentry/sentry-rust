3af52c61d4db83612acd6893cf2dfda3cf236836
Arpad Borsos <arpad.borsos@googlemail.com>
2020-04-20 14:29:11 +0200

    ref: Add a new implementation for the Client

--- sentry-core/src/client.rs
+++ sentry-core/src/client.rs
@@ -1,4 +1,5 @@
 use crate::{ClientOptions, Event, Scope, Uuid};
+use std::time::Duration;
 
 /// TODO
 #[derive(Clone, Debug)]
@@ -13,6 +14,23 @@ impl Client {
     /// registered event processors.
     pub fn capture_event(&self, event: Event<'static>, scope: Option<&Scope>) -> Option<Uuid> {
         let _ = (event, scope);
+        todo!()
+    }
+
+    /// Flush all pending events.
+    ///
+    /// This returns `true` if the queue was successfully flushed in the
+    /// given time or `false` if not (for instance because of a timeout).
+    pub fn flush(&self, timeout: Duration) -> bool {
+        let _ = timeout;
+        todo!()
+    }
+
+    /// Close and drop the client, flushing all pending events.
+    ///
+    /// See [`Client::flush`] for more Information.
+    pub fn close(self, timeout: Option<Duration>) -> bool {
+        let _ = timeout;
         todo!()
     }
 }
