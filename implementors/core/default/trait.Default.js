3ae801b8002f5439ba76eeb7adcd0469ecf5a39b
Arpad Borsos <arpad.borsos@sentry.io>
2020-05-14 15:50:55 +0200

    craft(gh-pages): update, version "0.18.1"

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["sentry"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sentry/struct.Scope.html\" title=\"struct sentry::Scope\">Scope</a>","synthetic":false,"types":["sentry::scope::real::Scope"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sentry/internals/struct.ScopeGuard.html\" title=\"struct sentry::internals::ScopeGuard\">ScopeGuard</a>","synthetic":false,"types":["sentry::scope::real::ScopeGuard"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sentry/struct.ClientOptions.html\" title=\"struct sentry::ClientOptions\">ClientOptions</a>","synthetic":false,"types":["sentry::client::ClientOptions"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
