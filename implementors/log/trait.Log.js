59ea080809c3d2718b93f5d90ed62db18ab3da00
Arpad Borsos <arpad.borsos@sentry.io>
2020-08-28 11:14:20 +0200

    craft(gh-pages): update, version "0.19.1"

--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["sentry_log"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.8/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"sentry_log/struct.Logger.html\" title=\"struct sentry_log::Logger\">Logger</a>","synthetic":false,"types":["sentry_log::logger::Logger"]}];
+implementors["sentry_log"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.11/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"sentry_log/struct.Logger.html\" title=\"struct sentry_log::Logger\">Logger</a>","synthetic":false,"types":["sentry_log::logger::Logger"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
