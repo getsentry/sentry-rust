09932f075459cc2bf6b4b153a4ae91114363f185
Jake Shadle <jake.shadle@embark-studios.com>
2020-11-24 12:28:32 +0100

    Debugging...

--- sentry-core/src/macros.rs
+++ sentry-core/src/macros.rs
@@ -55,17 +55,18 @@ macro_rules! with_client_impl {
 #[doc(hidden)]
 macro_rules! sentry_debug {
     ($($arg:tt)*) => {
-        #[cfg(feature = "debug-logs")] {
-            ::log_::debug!(target: "sentry", $($arg)*);
-        }
-        #[cfg(not(feature = "debug-logs"))] {
-            $crate::Hub::with(|hub| {
-                if hub.client().map_or(false, |c| c.options().debug) {
-                    eprint!("[sentry] ");
-                    eprintln!($($arg)*);
-                }
-            });
-        }
+        eprintln!($($arg)*);
+        // #[cfg(feature = "debug-logs")] {
+        //     ::log_::debug!(target: "sentry", $($arg)*);
+        // }
+        // #[cfg(not(feature = "debug-logs"))] {
+        //     $crate::Hub::with(|hub| {
+        //         if hub.client().map_or(false, |c| c.options().debug) {
+        //             eprint!("[sentry] ");
+        //             eprintln!($($arg)*);
+        //         }
+        //     });
+        // }
     }
 }
 
