c63510352e1d8f974be61c7454e9c6758c42b59a
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-07-08 10:59:29 +0200

    Add an additional gate on `target_arch=aarch64`

--- sentry-core/src/profiling.rs
+++ sentry-core/src/profiling.rs
@@ -183,9 +183,9 @@ fn get_profile_from_report(
             .frames
             .iter()
             .map(|frame| RustFrame {
-                #[cfg(target_os = "macos")]
+                #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
                 instruction_addr: format!("{:p}", frame.ip as *mut core::ffi::c_void),
-                #[cfg(not(target_os = "macos"))]
+                #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
                 instruction_addr: format!("{:p}", frame.ip()),
             })
             .collect();
