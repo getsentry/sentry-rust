bde7b643a134aa64da8c7a370b47c9040d8cfdef
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-07-08 11:15:59 +0200

    fix(profiling): Add support to use frame-pointer on macos (#481)
    * Add support to use frame-pointer on macos with `target_arch=aarch64`
    
    This solves a problem (probably due to unwind) on macos, where the profiler would
    yield a `Trace/BPT trap`.
    
    Instead of using the `backtrace` crate, when running on `macos`, using `frame-pointer`
    is more recommended to get the backtrace.

--- sentry-core/src/profiling.rs
+++ sentry-core/src/profiling.rs
@@ -183,6 +183,9 @@ fn get_profile_from_report(
             .frames
             .iter()
             .map(|frame| RustFrame {
+                #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+                instruction_addr: format!("{:p}", frame.ip as *mut core::ffi::c_void),
+                #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
                 instruction_addr: format!("{:p}", frame.ip()),
             })
             .collect();
