ba4d49c15de25c502e97c0c7d07df9d38b76a337
Francesco Vigliaturo <viglia90@gmail.com>
2023-01-26 11:10:30 +0100

    Add pid check

--- sentry-core/src/profiling.rs
+++ sentry-core/src/profiling.rs
@@ -307,7 +307,7 @@ fn collect_samples(
             for thread in threads.iter() {
                 let tid = thread.id().unwrap();
                 let timestamp = SystemTime::now();
-                if tid == collector_tid {
+                if collector_tid == tid || collector_tid == pid {
                     continue;
                 }
                 println!("collecting stack for thread: {}", tid);
