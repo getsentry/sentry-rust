ab6c40fb31eec6181b18330df3d1b5c51dc10043
Max Bruckner <max@lumeo.com>
2023-08-28 09:45:43 +0200

    Update webpki-roots to fix RUSTSEC-2023-0052
    webpki-roots 0.23 still depends on webpki 0.22 which is affected by a
    denial of service vulnerability, see https://rustsec.org/advisories/RUSTSEC-2023-0052.html

--- sentry/src/transports/ureq.rs
+++ sentry/src/transports/ureq.rs
@@ -74,7 +74,7 @@ impl UreqHttpTransport {
                     }
 
                     let mut root_store = RootCertStore::empty();
-                    root_store.add_server_trust_anchors(TLS_SERVER_ROOTS.0.iter().map(|ta| {
+                    root_store.add_trust_anchors(TLS_SERVER_ROOTS.iter().map(|ta| {
                         OwnedTrustAnchor::from_subject_spki_name_constraints(
                             ta.subject,
                             ta.spki,
