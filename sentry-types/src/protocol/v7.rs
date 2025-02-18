248de09e183481ae8c0186b246aec8fffcf22979
Zach Mitchell <zmitchell@fastmail.com>
2025-02-17 20:26:32 -0700

    feat: add feedback context

--- sentry-types/src/protocol/v7.rs
+++ sentry-types/src/protocol/v7.rs
@@ -1098,6 +1098,8 @@ pub enum Context {
     Trace(Box<TraceContext>),
     /// GPU data
     Gpu(Box<GpuContext>),
+    /// User feedback
+    Feedback(Box<FeedbackContext>),
     /// Generic other context data.
     #[serde(rename = "unknown")]
     Other(Map<String, Value>),
@@ -1114,6 +1116,7 @@ impl Context {
             Context::Browser(..) => "browser",
             Context::Trace(..) => "trace",
             Context::Gpu(..) => "gpu",
+            Context::Feedback(..) => "feedback",
             Context::Other(..) => "unknown",
         }
     }
@@ -1330,6 +1333,17 @@ pub struct GpuContext {
     /// Additional arbitrary fields for forwards compatibility.
     #[serde(flatten)]
     pub other: Map<String, Value>,
+}
+
+/// User feedback context describes the feedback and who sent it
+#[derive(Serialize, Deserialize, Debug, Clone, Default, PartialEq)]
+pub struct FeedbackContext {
+    /// The user's contact email
+    pub contact_email: String,
+    /// The feedback from the user
+    pub message: String,
+    /// The user's name
+    pub name: String,
 }
 
 /// Holds the identifier for a Span
@@ -1644,6 +1658,9 @@ pub struct Event<'a> {
     /// SDK metadata
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub sdk: Option<Cow<'a, ClientSdkInfo>>,
+    /// The event type
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub r#type: Option<String>,
 }
 
 impl Default for Event<'_> {
@@ -1676,6 +1693,7 @@ impl Default for Event<'_> {
             extra: Default::default(),
             debug_meta: Default::default(),
             sdk: Default::default(),
+            r#type: Default::default(),
         }
     }
 }
@@ -1721,6 +1739,7 @@ impl<'a> Event<'a> {
             extra: self.extra,
             debug_meta: Cow::Owned(self.debug_meta.into_owned()),
             sdk: self.sdk.map(|x| Cow::Owned(x.into_owned())),
+            r#type: self.r#type,
         }
     }
 }
