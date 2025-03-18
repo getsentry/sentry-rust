eb247f74b1bee4ea756d369cb2394f2560d59d49
Jack Lavigne <jacklavigne00@gmail.com>
2025-03-18 15:25:29 +0100

    fix: don't import `SessionMode` on non-release health features

--- sentry/src/init.rs
+++ sentry/src/init.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use sentry_core::{sentry_debug, SessionMode};
+use sentry_core::sentry_debug;
+#[cfg(feature = "release-health")]
+use sentry_core::SessionMode;
 
 use crate::defaults::apply_defaults;
 use crate::{Client, ClientOptions, Hub};
