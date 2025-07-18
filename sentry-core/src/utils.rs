9e0e0336a5ac518730058bb3a039932d923dd041
lcian <lorenzo.cian@sentry.io>
2025-07-18 15:11:04 +0200

    feat: filter username and password in URLs

--- sentry-core/src/utils.rs
+++ sentry-core/src/utils.rs
@@ -1,5 +1,7 @@
 //! Utilities reused across dependant crates and integrations.
 
+use url::Url;
+
 const SENSITIVE_HEADERS_UPPERCASE: &[&str] = &[
     "AUTHORIZATION",
     "PROXY_AUTHORIZATION",
@@ -10,8 +12,20 @@ const SENSITIVE_HEADERS_UPPERCASE: &[&str] = &[
     "X_API_KEY",
 ];
 
+const PII_REPLACEMENT: &str = "[Filtered]";
+
 /// Determines if the HTTP header with the given name shall be considered as potentially carrying
 /// sensitive data.
 pub fn is_sensitive_header(name: &str) -> bool {
     SENSITIVE_HEADERS_UPPERCASE.contains(&name.to_ascii_uppercase().replace("-", "_").as_str())
+}
+
+/// Scrub PII (username and password) from the given URL.
+pub fn scrub_pii_from_url(url: &mut Url) {
+    if !url.username().is_empty() {
+        let _ = url.set_username(PII_REPLACEMENT);
+    }
+    if url.password().is_some() {
+        let _ = url.set_password(Some(PII_REPLACEMENT));
+    }
 }
