3d67750c6271412583a964c650f157aaa99f0b8f
Daniel Szoke <daniel.szoke@sentry.io>
2026-04-17 16:27:43 +0200

    wip cleanup

--- sentry-core/src/metrics.rs
+++ sentry-core/src/metrics.rs
@@ -1,107 +1,84 @@
 //! Macros for Sentry [trace metrics](https://develop.sentry.dev/sdk/telemetry/metrics/).
 
+/// Internal helper for metric construction and emission. Not part of the public API.
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __metric_capture {
+    ($type:expr, $name:expr, $value:expr, $unit:expr, $attributes:expr) => {{
+        let metric = $crate::protocol::Metric {
+            r#type: $type,
+            name: $name.to_owned().into(),
+            value: $value as f64,
+            timestamp: ::std::time::SystemTime::now(),
+            trace_id: $crate::protocol::TraceId::default(),
+            span_id: None,
+            unit: $unit,
+            attributes: $attributes,
+        };
+        $crate::Hub::current().capture_metric(metric)
+    }};
+}
+
 /// Internal macro support for metric emission. Not part of the public API.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __metric_emit {
-    // Name, value, and explicit unit
-    ($type:expr, $name:expr, $value:expr, $unit:expr) => {{
-        let metric = $crate::protocol::Metric {
-            r#type: $type,
-            name: $name.to_owned().into(),
-            value: $value as f64,
-            timestamp: ::std::time::SystemTime::now(),
-            trace_id: $crate::protocol::TraceId::default(),
-            span_id: None,
-            unit: Some(($unit).into()),
-            attributes: $crate::protocol::Map::new(),
-        };
-        $crate::Hub::current().capture_metric(metric)
+    ($type:expr, $name:expr, $value:expr $(, $($rest:tt)+)?) => {{
+        $crate::__metric_emit!(@dispatch $type, $name, $value $(, $($rest)+)?)
     }};
 
-    // Name, value, explicit unit, and attributes
-    ($type:expr, $name:expr, $value:expr, $unit:expr, $($rest:tt)+) => {{
-        let mut attributes = $crate::protocol::Map::new();
-        let unit = Some(($unit).into());
-        $crate::__metric_emit!(@internal attributes, unit, $type, $name, $value, $($rest)+)
-    }};
-
-    // Name and value only
-    (@no_unit $type:expr, $name:expr, $value:expr) => {{
-        let metric = $crate::protocol::Metric {
-            r#type: $type,
-            name: $name.to_owned().into(),
-            value: $value as f64,
-            timestamp: ::std::time::SystemTime::now(),
-            trace_id: $crate::protocol::TraceId::default(),
-            span_id: None,
-            unit: None,
-            attributes: $crate::protocol::Map::new(),
-        };
-        $crate::Hub::current().capture_metric(metric)
-    }};
-
-    // Name and value with attributes
-    (@no_unit $type:expr, $name:expr, $value:expr, $($rest:tt)+) => {{
+    (@no_unit $type:expr, $name:expr, $value:expr $(, $($rest:tt)+)?) => {{
         let mut attributes = $crate::protocol::Map::new();
         let unit = None;
-        $crate::__metric_emit!(@internal attributes, unit, $type, $name, $value, $($rest)+)
+        $crate::__metric_emit!(@attrs attributes, unit, $type, $name, $value $(, $($rest)+)?)
     }};
 
-    // Recursive case: quoted key = value, followed by more
-    (@internal $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $key:literal = $aval:expr, $($rest:tt)+) => {{
+    (@dispatch $type:expr, $name:expr, $value:expr) => {{
+        $crate::__metric_capture!(
+            $type,
+            $name,
+            $value,
+            None,
+            $crate::protocol::Map::new()
+        )
+    }};
+
+    (@dispatch $type:expr, $name:expr, $value:expr, $key:literal = $aval:expr $(, $($rest:tt)+)?) => {{
+        let mut attributes = $crate::protocol::Map::new();
+        let unit = None;
+        $crate::__metric_emit!(@attrs attributes, unit, $type, $name, $value, $key = $aval $(, $($rest)+)?)
+    }};
+
+    (@dispatch $type:expr, $name:expr, $value:expr, $($key:ident).+ = $aval:expr $(, $($rest:tt)+)?) => {{
+        let mut attributes = $crate::protocol::Map::new();
+        let unit = None;
+        $crate::__metric_emit!(@attrs attributes, unit, $type, $name, $value, $($key).+ = $aval $(, $($rest)+)?)
+    }};
+
+    (@dispatch $type:expr, $name:expr, $value:expr, $unit:expr $(, $($rest:tt)+)?) => {{
+        let mut attributes = $crate::protocol::Map::new();
+        let unit = Some(($unit).into());
+        $crate::__metric_emit!(@attrs attributes, unit, $type, $name, $value $(, $($rest)+)?)
+    }};
+
+    (@attrs $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr) => {{
+        $crate::__metric_capture!($type, $name, $value, $unit, $attrs)
+    }};
+
+    (@attrs $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $key:literal = $aval:expr $(, $($rest:tt)+)?) => {{
         $attrs.insert(
             $key.to_owned().into(),
             $crate::protocol::LogAttribute($crate::protocol::Value::from($aval))
         );
-        $crate::__metric_emit!(@internal $attrs, $unit, $type, $name, $value, $($rest)+)
+        $crate::__metric_emit!(@attrs $attrs, $unit, $type, $name, $value $(, $($rest)+)?)
     }};
 
-    // Base case: quoted key = value (last pair)
-    (@internal $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $key:literal = $aval:expr) => {{
-        $attrs.insert(
-            $key.to_owned().into(),
-            $crate::protocol::LogAttribute($crate::protocol::Value::from($aval))
-        );
-        let metric = $crate::protocol::Metric {
-            r#type: $type,
-            name: $name.to_owned().into(),
-            value: $value as f64,
-            timestamp: ::std::time::SystemTime::now(),
-            trace_id: $crate::protocol::TraceId::default(),
-            span_id: None,
-            unit: $unit,
-            attributes: $attrs,
-        };
-        $crate::Hub::current().capture_metric(metric)
-    }};
-
-    // Recursive case: ident key = value, followed by more
-    (@internal $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $($key:ident).+ = $aval:expr, $($rest:tt)+) => {{
+    (@attrs $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $($key:ident).+ = $aval:expr $(, $($rest:tt)+)?) => {{
         $attrs.insert(
             stringify!($($key).+).to_owned().into(),
             $crate::protocol::LogAttribute($crate::protocol::Value::from($aval))
         );
-        $crate::__metric_emit!(@internal $attrs, $unit, $type, $name, $value, $($rest)+)
-    }};
-
-    // Base case: ident key = value (last pair)
-    (@internal $attrs:ident, $unit:ident, $type:expr, $name:expr, $value:expr, $($key:ident).+ = $aval:expr) => {{
-        $attrs.insert(
-            stringify!($($key).+).to_owned().into(),
-            $crate::protocol::LogAttribute($crate::protocol::Value::from($aval))
-        );
-        let metric = $crate::protocol::Metric {
-            r#type: $type,
-            name: $name.to_owned().into(),
-            value: $value as f64,
-            timestamp: ::std::time::SystemTime::now(),
-            trace_id: $crate::protocol::TraceId::default(),
-            span_id: None,
-            unit: $unit,
-            attributes: $attrs,
-        };
-        $crate::Hub::current().capture_metric(metric)
+        $crate::__metric_emit!(@attrs $attrs, $unit, $type, $name, $value $(, $($rest)+)?)
     }};
 }
 
@@ -128,7 +105,7 @@ macro_rules! metric_count {
 /// Emits a gauge metric. Gauges represent current state (e.g., memory usage, pool size).
 ///
 /// Units are optional and, when provided, are passed as a positional argument
-/// after the value.
+/// after the value and before any attributes.
 ///
 /// Attributes can be passed with `key = value` or `"key" = value` syntax.
 ///
@@ -138,30 +115,21 @@ macro_rules! metric_count {
 /// use sentry::metric_gauge;
 ///
 /// metric_gauge!("memory.usage", 1024.0);
+/// metric_gauge!("memory.usage", 1024.0, host = "web-1", "cache.hit" = true);
 /// metric_gauge!("memory.usage", 1024.0, "byte");
-/// metric_gauge!("memory.usage", 1024.0, "byte", host = "web-1");
-/// metric_gauge!("memory.usage", 1024.0, unit = "attribute");
+/// metric_gauge!("memory.usage", 1024.0, "byte", host = "web-1", region.name = "us-east-1");
 /// ```
 #[macro_export]
 macro_rules! metric_gauge {
-    ($name:expr, $value:expr) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Gauge, $name, $value)
-    };
-    ($name:expr, $value:expr, $key:literal = $aval:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Gauge, $name, $value, $key = $aval $(, $($rest)+)?)
-    };
-    ($name:expr, $value:expr, $($key:ident).+ = $aval:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Gauge, $name, $value, $($key).+ = $aval $(, $($rest)+)?)
-    };
-    ($name:expr, $value:expr, $unit:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!($crate::protocol::MetricType::Gauge, $name, $value, $unit $(, $($rest)+)?)
+    ($name:expr, $value:expr $(, $($rest:tt)+)?) => {
+        $crate::__metric_emit!($crate::protocol::MetricType::Gauge, $name, $value $(, $($rest)+)?)
     };
 }
 
 /// Emits a distribution metric. Distributions measure statistical spread (e.g., response times).
 ///
 /// Units are optional and, when provided, are passed as a positional argument
-/// after the value.
+/// after the value and before any attributes.
 ///
 /// Attributes can be passed with `key = value` or `"key" = value` syntax.
 ///
@@ -171,21 +139,13 @@ macro_rules! metric_gauge {
 /// use sentry::metric_distribution;
 ///
 /// metric_distribution!("response.time", 150.0);
-/// metric_distribution!("response.time", 150.0, "millisecond", route = "/users");
-/// metric_distribution!("response.time", 150.0, unit = "attribute");
+/// metric_distribution!("response.time", 150.0, route = "/users", http.status_code = 200);
+/// metric_distribution!("response.time", 150.0, "millisecond");
+/// metric_distribution!("response.time", 150.0, "millisecond", route = "/users", "http.status_code" = 200);
 /// ```
 #[macro_export]
 macro_rules! metric_distribution {
-    ($name:expr, $value:expr) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Distribution, $name, $value)
-    };
-    ($name:expr, $value:expr, $key:literal = $aval:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Distribution, $name, $value, $key = $aval $(, $($rest)+)?)
-    };
-    ($name:expr, $value:expr, $($key:ident).+ = $aval:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!(@no_unit $crate::protocol::MetricType::Distribution, $name, $value, $($key).+ = $aval $(, $($rest)+)?)
-    };
-    ($name:expr, $value:expr, $unit:expr $(, $($rest:tt)+)?) => {
-        $crate::__metric_emit!($crate::protocol::MetricType::Distribution, $name, $value, $unit $(, $($rest)+)?)
+    ($name:expr, $value:expr $(, $($rest:tt)+)?) => {
+        $crate::__metric_emit!($crate::protocol::MetricType::Distribution, $name, $value $(, $($rest)+)?)
     };
 }
